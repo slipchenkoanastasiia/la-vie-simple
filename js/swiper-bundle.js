bf1a1c3930abc86a5f3d280a1c912e6b99163e2c
slipchenkoanastasiia <138067087+slipchenkoanastasiia@users.noreply.github.com>
2024-02-28 00:19:55 +0200

    fix swiper

--- js/swiper-bundle.js
+++ js/swiper-bundle.js
@@ -1,13 +1,11 @@
-import Swiper from 'swiper';
-
-const swiper = new Swiper('.swiper-container', {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  loop: true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
+document.addEventListener('DOMContentLoaded', function() {
+  const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
 });
-
-export default swiper;
