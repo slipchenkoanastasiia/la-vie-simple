ff084a5f60144fcb1f3039bc47f4960e3aeddd0a
slipchenkoanastasiia <138067087+slipchenkoanastasiia@users.noreply.github.com>
2024-04-19 12:39:39 +0300

    add header

--- js/onclick-color-svg.js
+++ js/onclick-color-svg.js
@@ -1,15 +1,15 @@
 function toggleColor(event) {
-    var svgIcon = event.target; // Отримуємо цільовий елемент, на якому спрацював обробник подій
-    var isWhite = svgIcon.classList.contains('white-fill');
+    let svgIcon = event.target; 
+    let isWhite = svgIcon.classList.contains('white-fill');
 
     if (isWhite) {
         svgIcon.classList.remove('white-fill');
-        svgIcon.style.fill = 'none'; // Перевірте, чи правильно задано заповнення SVG
-        svgIcon.style.stroke = 'var(--bg-color)'; // Повертаємо колір обрамлення
+        svgIcon.style.fill = 'none'; 
+        svgIcon.style.stroke = 'var(--bg-color)'; 
     } else {
         svgIcon.classList.add('white-fill');
-        svgIcon.style.fill = 'white'; // Задаємо білий колір для SVG
-        svgIcon.style.stroke = 'none'; // Видаляємо обрамлення
+        svgIcon.style.fill = 'white'; 
+        svgIcon.style.stroke = 'none'; 
     }
 }
 
