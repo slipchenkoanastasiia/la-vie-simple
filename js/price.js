37d35c2df1ab1d7678ed843311f2fd6d0ed86bf8
slipchenkoanastasiia <138067087+slipchenkoanastasiia@users.noreply.github.com>
2024-09-01 00:11:37 +0300

    fix sale new

--- js/price.js
+++ js/price.js
@@ -10,3 +10,25 @@ function setPrice(min, max) {
     document.getElementById('maxPrice').value = max === Infinity ? '' : max;
     updatePrice();
 }
+
+function increaseMinPrice() {
+    const minPriceInput = document.getElementById('minPrice');
+    let minPrice = parseInt(minPriceInput.value, 10);
+
+    if (!isNaN(minPrice)) {
+        minPrice += 1000; 
+        minPriceInput.value = minPrice;
+        updatePrice();
+    }
+}
+
+function decreaseMaxPrice() {
+    const maxPriceInput = document.getElementById('maxPrice');
+    let maxPrice = parseInt(maxPriceInput.value, 10);
+
+    if (!isNaN(maxPrice)) {
+        maxPrice -= 1000; 
+        maxPriceInput.value = maxPrice;
+        updatePrice();
+    }
+}
